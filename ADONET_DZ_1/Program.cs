c2d00d4cc4376a8176c28e6404166306bb9171a9
Rustam Galeev <Vincent-cat@mail.ru>
2019-04-22 02:14:43 +0600

    Добавьте файлы проекта.

---
+++ ADONET_DZ_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+
+namespace ADONET_DZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source = DESKTOP-OHQR73G;
+                                        Initial Catalog = ADO_Net_1_lesson;
+                                        Integrated Security = True";
+            string sqlExpression = "create table  grup (id int primary key identity, name nvarchar(50) not null)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();  
+                Console.WriteLine("Создана база данных");
+
+                sqlExpression = "insert into grup values ('Mike Tyson')";
+            
+                command = new SqlCommand(sqlExpression, connection);
+                int count = command.ExecuteNonQuery();
+                Console.WriteLine("Добавлено объектов: {0}", count);
+
+                sqlExpression = "select * from grup";
+            
+                command = new SqlCommand(sqlExpression, connection);
+                command.ExecuteNonQuery();
+            }
+            Console.Read();
+
+            
+            
+        }    
+    }
+}
